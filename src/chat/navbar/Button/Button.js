9ae75f797219c54199308ec5c19b8ac180f93bbc
datebackk <caskooss@gmail.com>
2021-04-10 23:47:56 +0300

    datebackk navbar buttons

--- src/chat/navbar/Button/Button.js
+++ src/chat/navbar/Button/Button.js
@@ -1,9 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./Button.scss"
 
-const Button = (props) => {
+const Button = ({icon, isActive}) => {
+    let mdiClass = ['mdi', 'nav__mdi'];
+    mdiClass.push(icon);
+
+    if (isActive) {
+        mdiClass.push('mdi--active');
+    }
+
     return (
-        <button className="tab-trigger"><i className="mdi nav__mdi mdi-account-outline"/></button>
+        <button className="tab-trigger"><i className={mdiClass.join(' ')}/></button>
     );
+};
+
+Button.prototype = {
+    icon: PropTypes.string.isRequired,
+    isActive: PropTypes.bool.isRequired
 }
 
 export default Button;
--- src/chat/navbar/Navbar.js
+++ src/chat/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Navbar.scss"
 import {useRecoilState} from "recoil";
 import {navBar} from "../../atom/globalState";
+import Button from "./Button/Button";
 
 const Navbar = (props) => {
 
@@ -10,9 +11,9 @@ const Navbar = (props) => {
     return (
         <section className="nav">
             <div className="nav__menu">
-                <a className="tab-trigger" href="#page1"><i className="mdi nav__mdi mdi-account-outline"/></a>
-                <a className="tab-trigger tab-trigger--active" href="#page2"><i className="mdi nav__mdi mdi-comment-outline"/></a>
-                <a className="tab-trigger" href="#page3"><i className="mdi nav__mdi mdi-account-multiple-plus-outline"/></a>
+                <Button icon={"mdi-account-outline"} isActive={false}/>
+                <Button icon={"mdi-comment-outline"} isActive={true}/>
+                <Button icon={"mdi-account-multiple-plus-outline"} isActive={false}/>
             </div>
         </section>
     );
